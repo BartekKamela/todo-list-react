43d908e2d9d360d4742e187fd4db30d12ba91bda
Bartlomiej Kamela <bartek@lubnet.pl>
2023-03-17 20:01:46 +0100

    changed header from H3 to H2

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,8 +2,8 @@ import "./style.css";
 
 const Section = ({title, content, extraHeaderContent}) => (
     <section className="section">
-        <header className="section__header section__header--modifier">
-          <h3 className="section__headerTitle">{title}</h3>
+        <header className="section__header">
+          <h2 className="section__headerTitle">{title}</h2>
           {extraHeaderContent}
         </header>
         <div className="section__content">
