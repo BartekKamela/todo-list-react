2ccbe29679e13411b212c96e49550c5d66ff3bf4
Bartlomiej Kamela <bartek@lubnet.pl>
2023-04-17 10:52:39 +0200

    transferring the initial useState to the function

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ import Form from "./Form";
 import Tasks from "./Tasks";
 import Menu from "./Menu";
 
+const getInitialTasks = () => {
+  const tasksFromLocalStorage = localStorage.getItem("tasks");
+
+  return tasksFromLocalStorage
+    ? JSON.parse(tasksFromLocalStorage)
+    : [];
+};
+
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
+  const [tasks, setTasks] = useState(getInitialTasks);
 
   useEffect (() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
