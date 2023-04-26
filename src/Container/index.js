6dfcdaa572afb63b74c4b540f164670c95564256
Bartlomiej Kamela <bartek@lubnet.pl>
2023-04-26 21:37:28 +0200

    moved styled components to index.js

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,4 +1,8 @@
-import { Main } from "./styled";
+import styled from "styled-components";
+
+const  Main = styled.main`
+    margin: 50px 20px 20px 20px;
+`;
 
 const Container = ({ children }) => (
     <Main>
--- src/Container/styled.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const  Main = styled.main`
-    margin: 50px 20px 20px 20px;
-`;
