359e185d70a3ec348eb47f1999b89a57159d6b0c
BartekKamela <bartek@lubnet.pl>
2025-10-18 16:21:05 +0200

    application split into  features

---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { useTasks } from "../../useTasks";
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+import Form from "./Form";
+import TasksList from "./TasksList";
+import Menu from "./Menu";
+
+function Tasks() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
+
+  const {
+    tasks,
+    removeTask,
+    toggleTaskDone,
+    setAllDone,
+    addNewTask,
+  } = useTasks();
+
+  return (
+    <Container>
+      <Header title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        content={<Form addNewTask={addNewTask} />}
+      />
+      <Section
+        title="Lista zadań"
+        content={
+          <TasksList
+            tasks={tasks}
+            hideDone={hideDone}
+            key={tasks.id}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
+        extraHeaderContent={
+          <Menu
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllDone={setAllDone}
+          />
+        }
+      />
+    </Container>
+  );
+}
+
+export default Tasks;
