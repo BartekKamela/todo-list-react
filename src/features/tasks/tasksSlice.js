7175cf92d6f9ca5858647c5ad381436a96828d62
BartekKamela <bartek@lubnet.pl>
2025-10-20 10:10:35 +0200

    formatted functions export

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -27,6 +27,12 @@ const tasksSlice = createSlice({
     },
 });
 
-export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone } = tasksSlice.actions;
+export const { 
+  addTask, 
+  toggleHideDone, 
+  toggleTaskDone, 
+  removeTask, 
+  setAllDone 
+} = tasksSlice.actions;
 export const selectTasks = state => state.tasks;
 export default tasksSlice.reducer;
