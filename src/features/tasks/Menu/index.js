ba864cb6f6c720ad13dc9e7df7f4132f741cb1d7
BartekKamela <bartek@lubnet.pl>
2025-10-20 09:27:51 +0200

    changed setAllDone form redux

--- src/features/tasks/Menu/index.js
+++ src/features/tasks/Menu/index.js
@@ -1,8 +1,8 @@
 import { useSelector, useDispatch } from "react-redux";
 import { Buttons, Button } from "./styled";
-import { selectTasks, toggleHideDone } from "../tasksSlice";
+import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
 
-const Menu = ({ setAllDone }) => {
+const Menu = () => {
     const {tasks, hideDone} = useSelector(selectTasks);
     const dispatch = useDispatch();
 
@@ -14,7 +14,7 @@ const Menu = ({ setAllDone }) => {
                     {hideDone ? "Pokaż" : "Ukryj"} ukończone
                 </Button>
                 <Button
-                onClick={setAllDone}
+                onClick={() => dispatch(setAllDone())}
                     disabled={tasks.every(({ done }) => done)}
                 >
                     Ukończ wszystkie
