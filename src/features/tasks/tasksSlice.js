86c7967af4f8098da544485f6560e8dbe153162b
BartekKamela <bartek@lubnet.pl>
2025-10-26 14:38:05 +0100

    added setTasks action

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -11,30 +11,35 @@ const tasksSlice = createSlice({
             tasks.push(task);
         },
         toggleHideDone: state => {
-          state.hideDone = !state.hideDone;
+            state.hideDone = !state.hideDone;
         },
         toggleTaskDone: ({ tasks }, { payload: taskId }) => {
-          const index = tasks.findIndex(({ id }) => id === taskId);
-          tasks[index].done = !tasks[index].done;
+            const index = tasks.findIndex(({ id }) => id === taskId);
+            tasks[index].done = !tasks[index].done;
         },
         removeTask: ({ tasks }, { payload: taskId }) => {
-          const index = tasks.findIndex(({ id }) => id === taskId);
-          tasks.splice(index, 1);
+            const index = tasks.findIndex(({ id }) => id === taskId);
+            tasks.splice(index, 1);
         },
-        setAllDone: ({ tasks })  => {
-          tasks.forEach((task) => task.done = true);
+        setAllDone: ({ tasks }) => {
+            tasks.forEach((task) => task.done = true);
         },
         fetchExampleTasks: () => { },
+        setTasks: (state, { payload: tasks }) => {
+            state.tasks = tasks;
+        },
+
     },
 });
 
-export const { 
-  addTask, 
-  toggleHideDone, 
-  toggleTaskDone, 
-  removeTask, 
-  setAllDone,
-  fetchExampleTasks,
+export const {
+    addTask,
+    toggleHideDone,
+    toggleTaskDone,
+    removeTask,
+    setAllDone,
+    fetchExampleTasks,
+    setTasks,
 } = tasksSlice.actions;
 
 export const selectTasksState = state => state.tasks;
