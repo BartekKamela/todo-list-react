bd27c4d49e3d7ebf86135f22bef8b881d17cf802
BartekKamela <bartek@lubnet.pl>
2025-10-27 18:34:11 +0100

    added Redirect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { HashRouter, Link, Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
+import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import Tasks from "./features/tasks/Tasks";
 import Author from "./features/author/Author";
 
 
 export default () => (
-  <HashRouter>
-    <nav>
-      <ul>
-        <li><Link to="/zadania">Zadania</Link></li>
-        <li><Link to="/autor">Autor</Link></li>
-      </ul>
-      <Switch>
-        <Route path="/zadania">
-          <Tasks />
-        </Route>
-        <Route path="/autor">
-          <Author />
-        </Route>
-      </Switch>
-    </nav>
-  </HashRouter>
+    <HashRouter>
+        <nav>
+            <ul>
+                <li><Link to="/zadania">Zadania</Link></li>
+                <li><Link to="/autor">Autor</Link></li>
+            </ul>
+            <Switch>
+                <Route path="/zadania">
+                    <Tasks />
+                </Route>
+                <Route path="/autor">
+                    <Author />
+                </Route>
+                <Route path="/">
+                    <Redirect to="/zadania" />
+                </Route>
+            </Switch>
+        </nav>
+    </HashRouter>
 );
