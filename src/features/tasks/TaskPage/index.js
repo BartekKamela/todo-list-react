9be547565befbf26f42aa88dae806cf06c3add01
BartekKamela <bartek@lubnet.pl>
2025-10-27 19:45:46 +0100

    added selector that retrieves the task by id

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -2,15 +2,18 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Container from "../../../common/Container";
 import Header from "../../../common/Header";
 import Section from "../../../common/Section";
+import { useSelector } from "react-redux";
+import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
     const { id } = useParams();
+    const task = useSelector(state => getTaskById(state, id));
 
     return (
         <Container>
             <Header title="Szczegóły zadania" />
             <Section
-                title={id}
+                title={task ? task.content : "Nie znaleziono zadania 😢"}
                 content={"nanana"}
             />
         </Container>
