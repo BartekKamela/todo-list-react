b3ff5596c4c6ad133ef41152ef1575f4ec9550e8
BartekKamela <bartek@lubnet.pl>
2025-10-27 18:49:30 +0100

    changed Link to NavLink

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import { HashRouter, NavLink, Switch, Route, Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import Tasks from "./features/tasks/Tasks";
 import Author from "./features/author/Author";
 
@@ -7,8 +7,8 @@ export default () => (
     <HashRouter>
         <nav>
             <ul>
-                <li><Link to="/zadania">Zadania</Link></li>
-                <li><Link to="/autor">Autor</Link></li>
+                <li><NavLink activeClassName="active" to="/zadania">Zadania</NavLink></li>
+                <li><NavLink activeClassName="active" to="/autor">Autor</NavLink></li>
             </ul>
             <Switch>
                 <Route path="/zadania">
