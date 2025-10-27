435a2f6b2ed68f5e473098a2c542ad1939a27fa3
BartekKamela <bartek@lubnet.pl>
2025-10-27 19:33:01 +0100

    added useParams from react-router

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,17 +1,20 @@
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Container from "../../../common/Container";
 import Header from "../../../common/Header";
 import Section from "../../../common/Section";
 
 function TaskPage() {
-  return (
-    <Container>
-      <Header title="Szczegóły zadania" />
-      <Section
-        title="Wkrótce"
-        content={"nanana"}
-      />
-    </Container>
-  );
+    const { id } = useParams();
+
+    return (
+        <Container>
+            <Header title="Szczegóły zadania" />
+            <Section
+                title={id}
+                content={"nanana"}
+            />
+        </Container>
+    );
 }
 
 export default TaskPage;
