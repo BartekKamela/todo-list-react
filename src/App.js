ce97cdb429387653fc058c4cdcd466bc704be73b
BartekKamela <bartek@lubnet.pl>
2025-10-27 18:05:41 +0100

    added navigation to react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
-import React from "react";
+import { HashRouter, Link, Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
 import Tasks from "./features/tasks/Tasks";
 
-export default () => <Tasks />;
+
+export default () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li><Link to="/zadania">Zadania</Link></li>
+      </ul>
+      <Switch>
+        <Route path="/zadania">
+          <Tasks />
+        </Route>
+      </Switch>
+    </nav>
+  </HashRouter>
+);
