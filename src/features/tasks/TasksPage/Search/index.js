47007e89031e3df3d0f05357af85f130541db3ae
BartekKamela <bartek@lubnet.pl>
2025-10-28 12:59:05 +0100

    added hook of query parameter

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,33 +1,27 @@
-import { useLocation, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import Input from "../../Input"
-import { Wrapper } from "./styled";
+import { useQueryParametr, useReplaceQueryParameter } from "../queryParameters";
 import searchQueryParamName from "../searchQueryParamName";
+import { Wrapper } from "./styled";
 
 
 export default () => {
-    const location = useLocation();
-    const history = useHistory();
-    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+    const query = useQueryParametr(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameter();
 
     const onInputChange = ({ target }) => {
-        const searchParams = new URLSearchParams(location.search);
-
-        if (target.value.trim() === "") {
-            searchParams.delete(searchQueryParamName);
-        } else {
-            searchParams.set(searchQueryParamName, target.value);
-        }
-
-        history.push(`${location.pathname}?${searchParams.toString()}`);
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim() !== "" ? target.value : undefined,
+        });
     };
 
-    return (
-        <Wrapper>
-            <Input
-                placeholder="Filtruj zadania"
-                value={query || ""}
-                onChange={onInputChange}
-            />
-        </Wrapper>
-    )
-};
+        return (
+            <Wrapper>
+                <Input
+                    placeholder="Filtruj zadania"
+                    value={query || ""}
+                    onChange={onInputChange}
+                />
+            </Wrapper>
+        )
+    };
