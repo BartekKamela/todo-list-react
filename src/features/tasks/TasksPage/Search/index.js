0b798d34fe6228af096b93abe642dc4de5096454
BartekKamela <bartek@lubnet.pl>
2025-10-28 07:15:25 +0100

    added Wrapper to the search component

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,5 +1,6 @@
 import { useLocation, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import Input from "../../Input"
+import { Wrapper } from "./styled";
 
 
 export default () => {
@@ -7,10 +8,10 @@ export default () => {
     const history = useHistory();
     const query = (new URLSearchParams(location.search)).get("szukaj");
 
-    const onInputChange = ({target}) => {
+    const onInputChange = ({ target }) => {
         const searchParams = new URLSearchParams(location.search);
 
-        if(target.value.trim() === "") {
+        if (target.value.trim() === "") {
             searchParams.delete("szukaj");
         } else {
             searchParams.set("szukaj", target.value);
@@ -20,10 +21,12 @@ export default () => {
     };
 
     return (
-        <Input 
-            placeholder="Filtruj zadania"
-            value={query || ""}
-            onChange={onInputChange}
-        />
+        <Wrapper>
+            <Input
+                placeholder="Filtruj zadania"
+                value={query || ""}
+                onChange={onInputChange}
+            />
+        </Wrapper>
     )
 };
