e9d519fa7b29b55905a4489cbe0383602c382e89
BartekKamela <bartek@lubnet.pl>
2025-10-28 07:09:20 +0100

    added useHistory hook and function onInputCHange

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,14 +1,29 @@
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { useLocation, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import Input from "../../Input"
+
 
 export default () => {
     const location = useLocation();
+    const history = useHistory();
     const query = (new URLSearchParams(location.search)).get("szukaj");
+
+    const onInputChange = ({target}) => {
+        const searchParams = new URLSearchParams(location.search);
+
+        if(target.value.trim() === "") {
+            searchParams.delete("szukaj");
+        } else {
+            searchParams.set("szukaj", target.value);
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString()}`);
+    };
 
     return (
         <Input 
             placeholder="Filtruj zadania"
             value={query || ""}
+            onChange={onInputChange}
         />
     )
 };
